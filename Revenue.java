562fa68fcc5aababa3886dcba4bf0a15b01f9da5
Toska12138 <56856657+Toska12138@users.noreply.github.com>
2021-10-14 09:20:22 +0800

    Revenue Functions Completed

--- Revenue.java
+++ Revenue.java
@@ -6,6 +6,12 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Revenue {
+    /**
+     * orders:
+     *      ArrayList of orders
+     * saleItems:
+     *      HashMap<Food_object, quantities> of individual saleItems
+     */
     private ArrayList<Order> orders;
     private HashMap<Food, Integer> saleItems;
 
@@ -13,37 +19,54 @@ public class Revenue {
         this.orders = orders;
         this.saleItems = h;
     }
-    public void generateRevenueReport(LocalDateTime start, LocalDateTime end) {
-        System.out.println("");
+
+    public void printRevenueReport(LocalDateTime start, LocalDateTime end) {
+        Revenue temp = this.cut(start, end);
+        double total = 0;
+        for(Order o: temp.getOrders()){
+            total += o.subTotal;
+            System.out.println(o.subTotal);
+            /**
+             * sth to print
+             */
+        }
     }
 
-    public ArrayList<Order> getOrders() {
-        return orders;
-    }
-
-    public HashMap<Food, Integer> getSaleItems() {
-        return saleItems;
+    public void printRevenueReport() {
+        double total = 0;
+        for(Order o: this.getOrders()){
+            total += o.subTotal;
+            System.out.println(o.subTotal);
+            /**
+             * sth to print
+             */
+        }
     }
 
     public double getTotalRevenue(LocalDateTime start, LocalDateTime end) {
-        return Double.MIN_VALUE;
+        return this.cut(start, end).getOrders().stream().mapToDouble(o -> o.subTotal).sum();
     }
 
-    public void clearOrders() {
+    public double getTotalRevenue() {
+        return this.getOrders().stream().mapToDouble(o -> o.subTotal).sum();
+    }
 
+    public void clear() {
+        this.getOrders().clear();
+        this.getSaleItems().clear();
     }
 
     public void addOrder(Order o) {
-        orders.add(o);
+        this.getOrders().add(o);
         for (Map.Entry<Food, Integer> entry : o.items.entrySet()) {
             Food key = entry.getKey();
             int value = entry.getValue();
-            saleItems.put(key, value + saleItems.get(key));
+            this.getSaleItems().put(key, value + getSaleItems().getOrDefault(key, 0));
         }
     }
 
     public void removeOrder(int orderId) {
-        orders.removeIf(o -> (o.id == orderId));
+        this.getOrders().removeIf(o -> (o.id == orderId));
     }
 
     public ArrayList<Order> sortOrders(boolean byDate) {
@@ -52,7 +75,7 @@ public class Revenue {
          *      True: sort by datetime
          *      False: sort by stuff
          */
-        ArrayList<Order> temp = new ArrayList<>(this.orders);
+        ArrayList<Order> temp = new ArrayList<>(this.getOrders());
         if(byDate){
             temp.sort((a, b) -> a.time.compareTo(b.time));
         }
@@ -63,8 +86,27 @@ public class Revenue {
     }
 
     private Revenue cut(LocalDateTime start, LocalDateTime end){
-        ArrayList<Order> orders = this.orders.stream().filter(o -> (o.time.isAfter(start) && o.time.isBefore(end))).collect(Collectors.toCollection(ArrayList::new));
+        /**
+         * return:
+         *      new Revenue within "start" and "end"
+         */
+        ArrayList<Order> new_orders = this.getOrders().stream().filter(o -> (o.time.isAfter(start) && o.time.isBefore(end))).collect(Collectors.toCollection(ArrayList::new));
+        HashMap<Food, Integer> new_saleItems = new HashMap<>();
+        for(Order o: new_orders){
+            for (Map.Entry<Food, Integer> entry : o.items.entrySet()) {
+                Food key = entry.getKey();
+                int value = entry.getValue();
+                new_saleItems.put(key, value + this.getSaleItems().getOrDefault(key, 0));
+            }
+        }
+        return new Revenue(new_orders, new_saleItems);
+    }
 
-        return this;
+    public ArrayList<Order> getOrders() {
+        return this.orders;
+    }
+
+    public HashMap<Food, Integer> getSaleItems() {
+        return this.saleItems;
     }
 }
